a5afde2e83560ab275ba0776739b0bc6820a0085
SantiagoGE1 <escagarcisanti@gmail.com>
2024-11-30 12:04:40 -0400

    post create

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView 
 from .models import Post
 # Create your views here.
 class PostListView(ListView):
@@ -8,4 +9,9 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'content', 'author']
     
